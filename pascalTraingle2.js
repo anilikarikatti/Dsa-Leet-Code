e51801130d3261c6d8573ceba699b23d0e6f982d
anilikarikatti <anilkarikatti333@gmail.com>
2024-03-13 13:51:02 +0530

    pascal added to leet code

---
+++ pascalTraingle2.js
@@ -0,0 +1,25 @@
+function pascalTraingle(numberOfRows) {
+    let arr = [];
+  
+    for (let i = 0; i <=numberOfRows; i++) {
+      let tempArr = [];
+      for (let j = 0; j <= i; j++) {
+        if (j == 0 || j == i) {
+          tempArr.push(1);
+        } else {
+          let val = arr[i - 1];
+          let sum = val[j] + val[j - 1];
+          tempArr.push(sum);
+        }
+      }
+      arr.push(tempArr);
+      if(i == numberOfRows){
+        return tempArr
+      }
+    }
+  
+    // return arr;
+  }
+  
+  console.log(pascalTraingle(3));
+  
