c0a127fffd050178e7070e6affe55ece0b5fa388
anilak <48754895+anilikarikatti@users.noreply.github.com>
2024-08-24 22:27:00 +0530

    Add files via upload

---
+++ palindrome.java
@@ -0,0 +1,27 @@
+import java.sql.Array;
+import java.util.Arrays;
+
+/**
+ * palindrome
+ */
+class Palindrome {
+
+    public static void main(String[] args) {
+        // check if number is palindrome or not
+
+        // StringBuilder str = new StringBuilder("gadag");
+
+        //  System.out.println(str.reverse() == str);
+        // if(str.reverse() == str){
+        //     System.out.println(str + " is palindrome");
+        // }else{
+        //     System.out.println(str + " is not palindrome" );
+        // }
+
+
+        String str = "gadag";
+
+        System.out.println(Arrays.toString(str.toCharArray()));
+    }
+    
+}
